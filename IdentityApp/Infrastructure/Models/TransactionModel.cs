853d957bbe2e6d85070176372d100e996aa284ab
Egor Yudin <eydnep2000@gmail.com>
2021-01-10 00:39:58 +0200

    TransactionModel was added.

---
+++ IdentityApp/Infrastructure/Models/TransactionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityApp.Infrastructure.Database.Enums;
+
+namespace IdentityApp.Infrastructure.Models
+{
+    public class TransactionModel
+    {
+        public int Id { get; set; }
+        public TransactionStatus Status { get; set; }
+        public TransactionType Type { get; set; }
+        public string ClientName { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
