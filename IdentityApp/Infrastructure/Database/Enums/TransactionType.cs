9682992f117c83462d0d8bf48b87c03749410135
Egor Yudin <eydnep2000@gmail.com>
2021-01-10 00:38:03 +0200

    TransactionType enum was added.

---
+++ IdentityApp/Infrastructure/Database/Enums/TransactionType.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace IdentityApp.Infrastructure.Database.Enums
+{
+    public enum TransactionType
+    {
+        [EnumMember(Value = "refill")]
+        Refill,
+        [EnumMember(Value = "withdrawal")]
+        Withdrawal,
+    }
+}
