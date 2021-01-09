49d46f59d7e136dc6bd1801472fe6c6cc92147c9
Egor Yudin <eydnep2000@gmail.com>
2021-01-10 00:40:06 +0200

    TransactionCsvModel was added.

---
+++ IdentityApp/Infrastructure/Models/TransactionCsvModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+using IdentityApp.Infrastructure.Database.Enums;
+using IdentityApp.Infrastructure.Helpers.CsvConverters;
+
+namespace IdentityApp.Infrastructure.Models
+{
+    public class TransactionCsvModel
+    {
+        [Name("TransactionId")]
+        public int Id { get; set; }
+        public TransactionStatus Status { get; set; }
+        public TransactionType Type { get; set; }
+        public string ClientName { get; set; }
+
+        [TypeConverter(typeof(DollarsConverter))]
+        public decimal Amount { get; set; }
+    }
+}
