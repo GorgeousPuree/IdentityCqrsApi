b188e130abd502be4f75531ea6ddfb29b45b787a
Egor Yudin <eydnep2000@gmail.com>
2021-01-10 00:37:51 +0200

    TransactionStatus enum was added.

---
+++ IdentityApp/Infrastructure/Database/Enums/TransactionStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace IdentityApp.Infrastructure.Database.Enums
+{
+    public enum TransactionStatus
+    {
+        [EnumMember(Value = "pending")]
+        Pending,
+        [EnumMember(Value = "completed")]
+        Completed,
+        [EnumMember(Value = "cancelled")]
+        Cancelled
+    }
+}
