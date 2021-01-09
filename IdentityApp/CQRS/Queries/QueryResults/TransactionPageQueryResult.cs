a5a0c544c554a6e695d6b99fc4210d741cb42abd
Egor Yudin <eydnep2000@gmail.com>
2021-01-10 00:45:22 +0200

    TransactionPageQueryResult was added.

---
+++ IdentityApp/CQRS/Queries/QueryResults/TransactionPageQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using IdentityApp.Infrastructure.Models;
+
+namespace IdentityApp.CQRS.Queries.QueryResults
+{
+    public class TransactionPageQueryResult
+    {
+        public List<TransactionModel> Transactions { get; set; }
+        public int TotalTransactionsCount { get; set; }
+
+        public TransactionPageQueryResult(int totalTransactionsCount, List<TransactionModel> transactions)
+        {
+            TotalTransactionsCount = totalTransactionsCount;
+            Transactions = transactions;
+        }
+    }
+}
