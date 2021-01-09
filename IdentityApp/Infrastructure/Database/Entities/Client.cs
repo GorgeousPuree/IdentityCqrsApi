92deb20729818568ec82c6890fecef64a32a704e
Egor Yudin <eydnep2000@gmail.com>
2021-01-10 00:38:28 +0200

    Transaction and Client entities were added.

---
+++ IdentityApp/Infrastructure/Database/Entities/Client.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.Infrastructure.Database.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        public List<Transaction> Transactions { get; set; }
+    }
+}
---
+++ IdentityApp/Infrastructure/Database/Entities/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using IdentityApp.Infrastructure.Database.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.Infrastructure.Database.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        [Required]
+        public TransactionStatus Status { get; set; }
+        [Required]
+        public TransactionType Type { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+    }
+}
