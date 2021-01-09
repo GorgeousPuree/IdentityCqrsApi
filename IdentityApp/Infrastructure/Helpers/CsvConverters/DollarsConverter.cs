5edeeee22253b2166b8df649918abda48ec94a09
Egor Yudin <eydnep2000@gmail.com>
2021-01-10 00:40:20 +0200

    DollarsConverter was added.

---
+++ IdentityApp/Infrastructure/Helpers/CsvConverters/DollarsConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace IdentityApp.Infrastructure.Helpers.CsvConverters
+{
+    public class DollarsConverter : DefaultTypeConverter
+    {
+        public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            return string.Format($"${value:0.00}");
+        }
+    }
+}
