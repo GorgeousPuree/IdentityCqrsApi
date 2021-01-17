5a3fec90f6057614f4b125da08404e58e0fb33e5
Egor Yudin <eydnep2000@gmail.com>
2021-01-17 14:24:20 +0200

    TransactionCsvMap was added.

---
+++ IdentityApp/Infrastructure/Models/TransactionCsvMap.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper.Configuration;
+using IdentityApp.Infrastructure.Database.Enums;
+using IdentityApp.Infrastructure.Helpers.CsvConverters;
+using System;
+
+namespace IdentityApp.Infrastructure.Models
+{
+    public class TransactionCsvMap : ClassMap<TransactionModel>
+    {
+        public TransactionCsvMap()
+        {
+            Map(m => m.Id).Name("TransactionId");
+            Map(m => m.Amount).TypeConverter<DollarsConverter>();
+            Map(m => m.Status).Validate(field =>
+            {
+                Enum.TryParse(typeof(TransactionStatus), field, out _);
+                return true;
+            });
+            Map(m => m.Type).Validate(field =>
+            {
+                Enum.TryParse(typeof(TransactionType), field, out _);
+                return true;
+            });
+            Map(m => m.ClientName).Name("ClientName");
+        }
+    }
+}
