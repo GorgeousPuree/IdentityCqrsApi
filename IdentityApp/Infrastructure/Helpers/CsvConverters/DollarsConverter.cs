47a7efabedb7149ca5ec2a0ef28a55a5ee7c2faf
Egor Yudin <eydnep2000@gmail.com>
2021-01-17 14:24:35 +0200

    DollarsConverter was updated.

--- IdentityApp/Infrastructure/Helpers/CsvConverters/DollarsConverter.cs
+++ IdentityApp/Infrastructure/Helpers/CsvConverters/DollarsConverter.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
-using CsvHelper;
+﻿using CsvHelper;
 using CsvHelper.Configuration;
 using CsvHelper.TypeConversion;
+using System;
 
 namespace IdentityApp.Infrastructure.Helpers.CsvConverters
 {
@@ -14,6 +10,13 @@ namespace IdentityApp.Infrastructure.Helpers.CsvConverters
         public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
         {
             return string.Format($"${value:0.00}");
+        }
+
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            var newStr = text.Replace("$", String.Empty);
+            //return base.ConvertFromString(text, row, memberMapData);
+            return decimal.Parse(newStr);
         }
     }
 }
